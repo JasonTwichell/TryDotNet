4917d29484415d9135f8733dd8ca6351630997ac
Jason Twichell <JasonTwichell@CoderFoundry.com>
2021-01-18 12:28:00 -0500

    first commit

---
+++ HelloWorld.md
@@ -0,0 +1,20 @@
+# Intro to Programming
+
+### Run your First Program
+Just click the `run` icon to get started
+
+``` cs --region intro --source-file .\HelloWorld\Program.cs --project .\HelloWorld\HelloWorld.csproj 
+Console.WriteLine("Hello World!");
+```
+
+# test-repo
+Just a repository for testing things
+
+## Link to section
+
+[link](HelloWorld.md)
+
+### HTML Link
+
+
+
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{  
+    class Program
+    {
+        // static void Main(string region = null,
+        //     string session = null,
+        //     string package = null,
+        //     string project = null,
+        //     string[] args = null)
+        // {
+        //     switch (region)
+        //     {
+        //         case "intro":
+        //             Intro();
+        //             break;               
+        //     }
+        // }
+    
+        public static void Intro()
+        {
+            #region intro
+            Console.WriteLine("Hello World!");
+            #endregion
+        }
+    }             
+}
